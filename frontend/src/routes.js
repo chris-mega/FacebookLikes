74dc1b606f81124243fe8ae17bf51ae2156719a4
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2020-05-26 22:27:22 -0500

    add react

---
+++ frontend/src/routes.js
@@ -0,0 +1,46 @@
+/*!
+
+=========================================================
+* Now UI Dashboard React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/now-ui-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Dashboard from "views/Dashboard.jsx";
+import TableList from "views/TableList.jsx";
+import UserPage from "views/UserPage.jsx";
+
+var dashRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "design_app",
+    component: Dashboard,
+    layout: "/admin"
+  },
+  {
+    path: "/user-page",
+    name: "User Profile",
+    icon: "users_single-02",
+    component: UserPage,
+    layout: "/admin"
+  },
+  {
+    
+    path: "/extended-tables",
+    name: "Table List",
+    icon: "files_paper",
+    component: TableList,
+    layout: "/admin"
+  },
+];
+export default dashRoutes;
